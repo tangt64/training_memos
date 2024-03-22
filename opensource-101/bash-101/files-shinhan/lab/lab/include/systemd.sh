c1e019cceafc3e04f5f900a299058cf9c67d086a
Choi Gook Hyun <bluehelix@gmail.com>
2024-03-22 14:47:11 +0900

    업데이트
    신한 예제

--- opensource-101/bash-101/files-shinhan/lab/lab/include/systemd.sh
+++ opensource-101/bash-101/files-shinhan/lab/lab/include/systemd.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+function StartService(){
+  local srv_name=$1
+  systemctl start $srv_name
+  systemctl is-active $srv_name
+}
+
+function StopService(){
+  local srv_name=$1
+  systemctl stop $srv_name
+  systemctl is-active $srv_name
+}
